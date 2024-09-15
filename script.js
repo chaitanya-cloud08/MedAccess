18ab0ac2ed46c36f938e7f9f3f211afaa72cc3de
chaitanya-cloud08 <chaitanyabehlforwork@gmail.com>
2024-09-15 17:16:55 +0530

    Added a new project

---
+++ script.js
@@ -0,0 +1,19 @@
+function book()
+{
+   window.location.href="appointment.html";
+}
+
+function faq()
+{
+ window.location.href="faq.html";
+}
+
+function profile()
+{
+ window.location.href="profile.html";
+}
+
+function sign()
+{
+   window.location.href="login.html";
+}
